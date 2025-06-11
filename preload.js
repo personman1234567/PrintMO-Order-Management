d0a9ca5521fc8c32b71c69b2ee8fc3d71860e17a
personman1234567 <ethan.duke@me.com>
2025-06-11 14:26:59 -0500

    Improved UI to help track status of orders

--- preload.js
+++ preload.js
@@ -5,5 +5,7 @@ contextBridge.exposeInMainWorld('api', {
   getQueue:      () => ipcRenderer.invoke('get-queue'),
   updateStatus: (orderId, status) => ipcRenderer.invoke('update-status', orderId, status),
   processBatch:  indices => ipcRenderer.invoke('process-batch', indices),
+  updateReady: (orderId, blanks, prints) =>
+    ipcRenderer.invoke('update-ready', orderId, blanks, prints),
   deleteOrder:   (orderName) => ipcRenderer.invoke('delete-order', orderName)
 });
